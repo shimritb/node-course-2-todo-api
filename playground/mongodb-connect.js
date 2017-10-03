2034222f431dc2aa2afae82d6b0b94a9b3d51fa2
shimritb <shimrit021@gmail.com>
2017-10-03 08:50:05 +0100

    Add update script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -34,5 +34,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
     // });
 
-    db.close();
+    // db.close();
+
+    
+    db.collection('Users').insertOne({
+     text: 'lisa',
+     age: 33   
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert todo', err);
+        }
+               console.log(JSON.stringify(result.ops, undefined, 2));
+    })
 });
